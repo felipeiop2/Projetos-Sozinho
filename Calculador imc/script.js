af484019b4c0b40ceb6255333721f62fa53d2427
felipeiop2 <154385972+felipeiop2@users.noreply.github.com>
2024-03-03 14:23:50 -0300

    Calculadora IMC

--- Calculador imc/script.js
+++ Calculador imc/script.js
@@ -5,8 +5,13 @@ function verificar(){
     let peso = window.document.getElementById('peso')
     let altura = window.document.getElementById('altura')
     let res = window.document.getElementById('res')
-    let res1 = Number(peso) / Number(altura)
-    let res2 = res1 / altura
-    res.innerHTML = (`Ola ${nome, sobrenome} seu IMC é de ${res2} `)
+    let res1 = parseFloat(peso.value) / parseFloat(altura.value)
+    
+    if(nome.value == "" || sobrenome.value == "" || peso.value == "" || altura.value == ""){
+        res.innerHTML(`<strong> Atenção alguns dados não foram prenchidos corretamente </strong>`)
+    } else{
+        let res2 = res1 / parseFloat(altura.value)
+        res.innerHTML(`Ola ${nome.value} ${sobrenome.value}, seu imc é de ${res2.toFixed(2)}`)
+    }
 
 }
